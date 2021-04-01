ac3c307dbb8d8b1eda074c194edbe6275e19fe6e
Gavin Asay <gavin.asay@gmail.com>
2021-04-01 01:22:59 -0600

    photolist test

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,26 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from '../../assets/small/commercial/0.jpg';
+import PhotoList from '../PhotoList';
 
-function Gallery(props) {
-	const [categories] = useState([
-		{
-			name: 'commercial',
-			description: 'Photos of grocery stores, food trucks, and other commercial projects',
-		},
-		{ name: 'portraits', description: 'Portraits of people in my life' },
-		{ name: 'food', description: 'Delicious delicacies' },
-		{ name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-	]);
-	const [currentCategory] = useState(categories[0]);
+function Gallery({ currentCategory }) {
+	const { name, description } = currentCategory;
 
 	return (
 		<section>
-			<h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-			<p>{capitalizeFirstLetter(currentCategory.description)}</p>
-			<div>
-				<img src={photo} alt='Commercial Example' className='img-thumbnail mx-1' />
-			</div>
+			<h1 data-testid='h1tag'>{capitalizeFirstLetter(name)}</h1>
+			<p>{capitalizeFirstLetter(description)}</p>
+			<PhotoList category={currentCategory.name} />
 		</section>
 	);
 }
